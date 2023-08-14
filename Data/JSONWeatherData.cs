526b020f48733a57781ce93e1c63351683991339
Nemat Mimi <nematmimi01@gmail.com>
2023-08-14 12:08:47 +0300

    Read JSON Format From User

--- Data/JSONWeatherData.cs
+++ Data/JSONWeatherData.cs
@@ -1,6 +1,13 @@
-﻿namespace Weather_Monitoring.Data
+﻿using Newtonsoft.Json;
+
+namespace Weather_Monitoring.Data
 {
     public class JSONWeatherData : WeatherData
     {
+        public static JSONWeatherData FromJson(string json)
+        {
+            var weatherData = JsonConvert.DeserializeObject<JSONWeatherData>(json);
+            return weatherData;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Weather_Monitoring
+﻿using Weather_Monitoring.Data;
+namespace Weather_Monitoring
 {
     public class Program
     {
         public static void Main()
         {
-
+            Console.WriteLine("Enter JSON data:");
+            var jsonInput = Console.ReadLine();
+            JSONWeatherData jsonweatherData = JSONWeatherData.FromJson(jsonInput);
+            Console.WriteLine($"Location: {jsonweatherData.Location}");
+            Console.WriteLine($"Temperature: {jsonweatherData.Temperature}");
+            Console.WriteLine($"Humidity: {jsonweatherData.Humidity}");
         }
     }
 }
