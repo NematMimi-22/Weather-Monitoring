75393ac4b7882a31517e82b62cf2ea3de9d85fca
Nemat Mimi <nematmimi01@gmail.com>
2023-09-06 12:55:24 +0300

    Remove Conflict

--- DataFormat/XmlParser.cs
+++ DataFormat/XmlParser.cs
@@ -1,21 +1,15 @@
 ﻿using System.Xml.Serialization;
 namespace Weather_Monitoring.DataFormat
 {
-    public class XmlParser
+    public class XmlParser 
     {
         public static WeatherData FromXml(string xml)
         {
             var serializer = new XmlSerializer(typeof(WeatherData));
-            try
+
+            using (TextReader reader = new StringReader(xml))
             {
-                using (TextReader reader = new StringReader(xml))
-                {
-                    return (WeatherData)serializer.Deserialize(reader);
-                }
-            } catch 
-            {
-                return null;
-            
+                return (WeatherData)serializer.Deserialize(reader);
             }
         }
     }
